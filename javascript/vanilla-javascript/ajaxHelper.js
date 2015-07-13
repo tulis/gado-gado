ba22e0b4745358e256ae8f8f04a3b90600a3b9b8
tulis <tanri.suryati@outlook.com>
2015-07-13 12:15:18 +1000

    add simple ajax helper

---
+++ javascript/vanilla-javascript/ajaxHelper.js
@@ -0,0 +1,65 @@
+function AjaxHelper() {
+    var httpRequest;
+    try {
+        httpRequest = new XMLHttpRequest();
+    } catch (exception) {
+        consoleLog(exception);
+    }
+
+    var onDone,
+        onFail,
+        readyState = {
+            uninitialized: 0,
+            loading: 1,
+            loaded: 2,
+            interactive: 3,
+            complete: 4
+        },
+        responseCode = {
+            ok: 200,
+            partialContent: 206,
+            movedPermanently: 301,
+            found: 302,
+            notFound: 404,
+            badRequest: 400,
+            forbidden: 403,
+            internalServerError: 500,
+            serviceUnavailable: 503
+        },
+        readyStateChange = function onReadyStateChange() {
+            try {
+                var responseData = httpRequest.responseText;
+                if (httpRequest.readyState === readyState.interactive
+                    && httpRequest.status === responseCode.ok) {
+                    var jsonResponseData = JSON.parse(responseData);
+
+                    onDone(jsonResponseData);
+                } else if (readyState.status !== undefined) {
+                    throw (readyState.status);
+                }
+            } catch (exception) {
+                consoleLog(exception);
+                onFail(exception);
+            }
+        };
+
+    this.get = function get(url, jsonData) {
+        try {
+            httpRequest.open("GET", url, true);
+            httpRequest.send(jsonData);
+            httpRequest.onreadystatechange = readyStateChange.bind(this);
+        } catch (exception) {
+            consoleLog(exception);
+            onFail(exception);
+        }
+        return this;
+    };
+    this.done = function done(todoFunct) {
+        onDone = todoFunct;
+        return this;
+    };
+    this.fail = function fail(todoFunct) {
+        onFail = todoFunct;
+        return this;
+    };
+}
