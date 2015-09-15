2c9e74a042d92314a5b94fd13a83407d7e3f5bc6
tulis <tanri.suryati@outlook.com>
2015-09-15 11:55:19 +1000

    Add a link on how to minimize the SQL Server RAM usage

--- sqlserver/getTotalMemoryUsageBySqlServer.sql
+++ sqlserver/getTotalMemoryUsageBySqlServer.sql
@@ -1,3 +1,5 @@
+-- Credit to: http://dba.stackexchange.com/questions/83618/sql-server-2008-how-much-ram-memory-should-sql-server-use-in-a-8gb-ram-server
+-- How to limit SQL Server RAM usage: http://www.brentozar.com/archive/2011/09/sysadmins-guide-microsoft-sql-server-memory/
 SELECT
 	(physical_memory_in_use_kb/1024) Memory_usedby_Sqlserver_MB,
 	(locked_page_allocations_kb/1024) Locked_pages_used_Sqlserver_MB,
