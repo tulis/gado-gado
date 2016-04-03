b9ebab434f2faae1912e6b7ee5fce2e371075907
tulis <tanri.suryati@outlook.com>
2016-04-03 15:02:57 +1000

    Rename to more meaningful name

--- nodejs/07-react-routes/routers/api/users.js
+++ nodejs/07-react-routes/routers/api/users.js
@@ -1,6 +1,6 @@
 module.exports.routers = module.exports.routers || {};
 module.exports.routers.api = module.exports.routers.api || {};
-module.exports.routers.api.users = (function exportsUsersApi(){
+module.exports.routers.api.users = (function exportsUsersApiRouter(){
 
     function listUsers(request, response){
         response.render("api/users", {});
