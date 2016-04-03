f7eeb2ae438c728da9e5d84fcf594879205347f0
tulis <tanri.suryati@outlook.com>
2016-04-03 14:20:23 +1000

    refactor react routes

---
+++ nodejs/07-react-routes/routers/api/users.js
@@ -0,0 +1,12 @@
+module.exports.routers = module.exports.routers || {};
+module.exports.routers.api = module.exports.routers.api || {};
+module.exports.routers.api.users = (function(){
+
+    function listUsers(request, response){
+        response.render("api/users", {});
+    };
+
+    return {
+        listUsers: listUsers
+    }
+})();
