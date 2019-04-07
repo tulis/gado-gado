133e04a0f67c2dfdff007b2c5456c68269498550
tulis <tanri.suryati@outlook.com>
2019-04-07 15:48:13 +1000

    Add null or empty sting guard on branch name

--- git/hooks/append-jira-id.ps1
+++ git/hooks/append-jira-id.ps1
@@ -5,12 +5,16 @@ echo $gitEditMessageFilePath
 $option = [System.StringSplitOptions]::RemoveEmptyEntries
 $branchName = (git rev-parse --abbrev-ref HEAD)
 
+if([string]::IsNullOrWhiteSpace($branchName)) {
+    exit 0
+}
 if($branchName -eq "HEAD"){
     # We do not want to fetch JIRA Summary if we are doing "git rebase -i"
     exit 0
 }
-$jiraId = (git rev-parse --abbrev-ref HEAD).Split("/", $option)[1]
-$jiraId = "$($jiraId.Split("-", $option)[0])-$($jiraId.Split("-", $option)[1])" 
+
+$jiraId = $branchName.Split("/", $option)[1]
+$jiraId = "$($jiraId.Split("-", $option)[0])-$($jiraId.Split("-", $option)[1])"
 
 $apiKey = cat “$env:UserProfile\.jira\api-key”
 $username = cat “$env:UserProfile\.jira\username”
@@ -23,9 +27,9 @@ $headers = @{
     Accept = "application/json"
 }
 $response = Invoke-WebRequest -Method 'Get' -uri "https://assetic.atlassian.net/rest/api/3/issue/$($jiraId)" -Headers $headers
-$content = ConvertFrom-Json $response.Content 
+$content = ConvertFrom-Json $response.Content
 
-# Example: 
+# Example:
 # CLD-4791 As a user, I want to ensure Disposed assets are not able to be modified
 $jiraTag = "`n`n`n$($content.key) $($content.fields.summary)"
 echo $jiraTag
