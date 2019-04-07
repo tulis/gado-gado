6d9f965cf517526101fb339ce16c217add7b7af2
tulis <tanri.suryati@outlook.com>
2019-04-08 00:20:47 +1000

    Implement Stop-Tracking Toggl

--- git/hooks/git-toggl.ps1
+++ git/hooks/git-toggl.ps1
@@ -55,7 +55,7 @@ function Get-JiraSummary([string]$jiraId, [hashtable]$jiraRequestHeaders){
 
 function Get-TogglRequestHeaders() {
     $apiKey = Get-Content “$env:UserProfile\.toggl\api-key”
-    $password = Get-Content “api_token”
+    $password = "api_token"
 
     # Powershell 5.1
     $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $apiKey,$password)))
@@ -67,34 +67,106 @@ function Get-TogglRequestHeaders() {
     return $headers
 }
 
-function Get-TogglePayload([string]$jiraSummary){
-    $projectId = 1099225
+function Start-Tracking([string] $jiraId){
+
+}
+
+function Stop-Tracking(){
+    [cmdletbinding(ConfirmImpact="Low")]
+    param(
+        [Parameter(Mandatory=$true, HelpMessage="Toggl Request Headers")]
+        [hashtable]$togglRequestHeaders,
+
+        [Parameter(Mandatory=$false, HelpMessage="local time (not UTC)")]
+        [DateTime] $endDateTime = (Get-Date).ToUniversalTime()
+    )
+    end {
+        Write-Verbose "$($PSBoundParameters | Out-String)"
+
+        $currentEntry = Invoke-WebRequest -Method 'Get' `
+            -uri "https://www.toggl.com/api/v8/time_entries/current" `
+            -Headers $togglRequestHeaders | ConvertFrom-Json
+
+        $entryId = $currentEntry.data.id
+        $payload = @{
+            "time_entry" = @{
+                "description" = $currentEntry.data.description
+                "duration" = (New-TimeSpan -Start $currentEntry.data.start.ToUniversalTime() -End $endDateTime).TotalSeconds
+                "stop" = $endDateTime.ToString("o")
+                "start" = $currentEntry.data.start.ToUniversalTime().ToString("o")
+                "pid" = $currentEntry.data.pid
+                "duronly" = $true
+                "created_with" = "powershell"
+            }
+        } | ConvertTo-Json -Depth 5
+
+        Write-Debug "Stopping [$entryId]...."
+        $response = Invoke-WebRequest -Method 'Put' `
+            -uri "https://www.toggl.com/api/v8/time_entries/$entryId/stop" `
+            -ContentType "application/json" `
+            -Headers $togglRequestHeaders
+
+        Write-Debug "Updating duration for [$entryId]..."
+        $response = Invoke-WebRequest -Method 'Put' `
+            -uri "https://www.toggl.com/api/v8/time_entries/$entryId" `
+            -ContentType "application/json" `
+            -Body $payload `
+            -Headers $togglRequestHeaders
+
+        Write-Debug $response
+        return $response | ConvertFrom-Json
+    }
+}
+
+function Start-Day(){
+
+}
+
+function Stop-Day(){
+
+}
+
+
+
+function Get-TogglePayload([int]$projectId, [string]$jiraSummary){
+
     $payload = @{
+        "time_entry" = @{
         "description" = $jiraSummary
         "duration" = 1000
         "start" = Get-Date -Format "o"
         "pid" = $projectId
         "created_with" = "powershell"
     }
+    }
     return $payload | ConvertTo-Json -Depth 5
 }
 
-function Add-TogglTimeEntry([hashtable]$togglRequestHeaders, [string] $togglPayload){
+function Add-TogglTimeEntry([Hashtable]$togglRequestHeaders, [string] $togglPayload){
+    Write-Host $togglRequestHeaders
+    Write-Host "Toggl Payload: $togglPayload"
+
+    #Write-Host (Invoke-WebRequest -Method Get -uri "" -Headers $togglRequestHeaders)
+
     $response = Invoke-WebRequest -Method 'Post' `
         -uri "https://www.toggl.com/api/v8/time_entries" `
         -ContentType "application/json" `
-        -Body $togglPayload
+        -Body $togglPayload `
         -Headers $togglRequestHeaders
 
     return $response
 }
 
+function Main(){
+
 $jiraId = Get-JiraId
 $jiraRequestHeaders = Get-JiraRequestHeaders
 $jiraSummary = Get-JiraSummary($jiraId, $jiraRequestHeaders)
 
-$togglRequestHeaders = Get-TogglRequestHeaders
-$togglPayload = Get-TogglePayload
-$togglCreateResponse = Add-TogglTimeEntry($togglRequestHeaders, $togglPayload)
+    # $togglRequestHeaders = Get-TogglRequestHeaders
+    # $togglPayload = Get-TogglePayload
+    # $togglCreateResponse = Add-TogglTimeEntry($togglRequestHeaders, $togglPayload)
 
-Write-Host $togglCreateResponse
+    # Write-Host $togglCreateResponse
+}
+
